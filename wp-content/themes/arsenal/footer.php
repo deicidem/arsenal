4cd9659cf50164cfde7681cac6c4dbfe7fb0e236
Анна Афанасьева <ann.afanaseva777@gmail.com>
2022-09-22 11:40:23 +0300

    22.09

--- wp-content/themes/arsenal/footer.php
+++ wp-content/themes/arsenal/footer.php
@@ -16,17 +16,27 @@
 	<div class="container">
 		<div class="footer-wrapper">
 			<div class="footer-block">
-				<div class="footer-block__title">Меню</div>
-				<div class="footer-block__content"></div>
+				<div class="footer-block__title">Телефон</div>
+				<a class="footer-block-item" href="tel:84956251580">+7 (495) 625-15-80</a>
+				<a class="footer-block-item" href="tel:84957902022">+7 (495) 790-20-22</a>
 			</div>
 			<div class="footer-block">
-				<div class="footer-block__title">Услуги</div>
-				<div class="footer-block__content"></div>
+				<div class="footer-block__title">Адрес</div>
+				<div class="footer-block-item">101000 г. Москва, ул. Маросейка д.3/13</div>
 			</div>
 			<div class="footer-block">
-				<div class="footer-block__title">Контакты</div>
-				<div class="footer-block__content"></div>
+				<div class="footer-block__title">Почта</div>
+				<div class="footer-block-item" href="mail:citi@citicleaning.ru">citi@citicleaning.ru</div>
 			</div>
+			<div class="footer-block">
+				<div class="footer-block-item">
+					<img src="<?php echo get_template_directory_uri() ?>/img/instagram.svg" alt="">
+					<img src="<?php echo get_template_directory_uri() ?>/img/vk.svg" alt="">
+				</div>
+			</div>
+		</div>
+		<div class="footer-wrapper-privacy">
+			© 2020, Клининговая компания «ARSENAL Cleaning» <a href="">Политика конфиденциальности</a>
 		</div>
 	</div>
 </footer><!-- #colophon -->
--- wp-content/themes/arsenal/index.php
+++ wp-content/themes/arsenal/index.php
@@ -50,17 +50,6 @@ get_header();
 		</div>
 	</section>
 
-	<section class="section form">
-		<div class="container">
-			<div class="form-wrapper">
-				<h2 class="form-title">Оставить заявку</h2>
-				<div class="form-content">
-				<?php echo do_shortcode('[contact-form-7 id="124" title="Contact form 1"]'); ?>
-				</div>
-			</div>
-		</div>
-	</section>
-
 	<section class="section">
 		<div class="container">
 			<div class="main-blue-card">
@@ -198,6 +187,52 @@ get_header();
 					</img>
 				</div>
 			</div>
+	</section>
+
+	<section class="section">
+		<div class="container">
+			<div class="form">
+				<div class="form-wrapper">
+					<h2 class="form-title">Оставить заявку</h2>
+					<div class="form-content">
+						<?php echo do_shortcode('[contact-form-7 id="124" title="Contact form 1"]'); ?>
+					</div>
+				</div>
+				<div class="form-right">
+					<h2 class="form-title form-title-blue">Контакты</h2>
+
+					<div class="form-right-wrapper">
+						<div class="form-right-wrapper-main">Если ищете исполнителя для <strong>регулярной</strong> профессиональной уборки коммерческих объектов – <strong>выгодно</strong> заключить договор!
+							Подписавшим соглашение клиентам дается <strong>скидка 30 % </strong>на мытье витрин.</div>
+						<div class="form-right-wrapper-text">
+							<div class="forn-right-subtitle"><strong>График работы</strong></div>
+							<div class="forn-right-subtitle">офиса: 10.00 - 19.00</div>
+							<div class="forn-right-subtitle">cотрудников: круглосуточно</div>
+						</div>
+						<div class="form-right-wrapper-text">
+							<div class="forn-right-subtitle"><strong>+7 (495) 625-15-80</strong></div>
+							<div class="forn-right-subtitle"><strong>+7 (495) 790-20-22</strong></div>
+						</div>
+						<div class="form-right-wrapper-text">
+							<div class="forn-right-subtitle"><strong>Электронная почта:</strong></div>
+							<div class="forn-right-subtitle">citi@citicleaning.ru</div>
+						</div>
+						<div class="form-right-wrapper-text">
+							<div class="form-right-link">
+								<img src="<?php echo get_template_directory_uri() ?>/img/instagram.svg" alt="">
+								<img src="<?php echo get_template_directory_uri() ?>/img/vk.svg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="form-right-img">
+						<img src="<?php echo get_template_directory_uri() ?>/img/form-img.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+		</div>
 	</section>
 
 	<section class="section reviews">
