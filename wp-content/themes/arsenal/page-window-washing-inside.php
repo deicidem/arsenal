426800f6e7e0e43af3bff464c29a8e5d70661b7c
deicidem <kronosdidi@gmail.com>
2022-09-25 22:01:29 +0300

    fix

--- wp-content/themes/arsenal/page-window-washing-inside.php
+++ wp-content/themes/arsenal/page-window-washing-inside.php
@@ -29,7 +29,6 @@ get_header();
 		<div class="container">
 			<h2 class="section-title"><?php the_title() ?></h2>
 			<?php the_content() ?>
-			<button class="button button-order">Заказать</button>
 		</div>
 	</section>
 </article><!-- #post-<?php the_ID(); ?> -->
